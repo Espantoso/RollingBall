dc1ca948af5860a032e94750acce24fa2a08abe7
AlexanderLeon <lav09.01.95@gmail.com>
2016-02-27 16:22:09 +0300

    test commit 2

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include "mainwindow.h"
 #include <QApplication>
 //#include "qml-box2d/box2dplugin.h"
+
+class test
+{
+    int tst;
+};
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
--- mainwindow.h
+++ mainwindow.h
@@ -1,9 +1,12 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 #include <QMainWindow>
-namespace Ui {
-class MainWindow;
+
+namespace Ui
+{
+    class MainWindow;
 }
+
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
